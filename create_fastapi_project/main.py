8c5da5432bb6913c28aed71bab933c2c0356c20b
Leonel Rivas <leonelrivas.rl@gmail.com>
2023-08-22 11:44:06 -0300

    fixed names of choices in create_project

--- create_fastapi_project/main.py
+++ create_fastapi_project/main.py
@@ -36,8 +36,15 @@ def create_project():
         validate=ProjectNameValidator,
         default="backend",
     ).ask()
+    basic = ITemplate.basic.value
+    langchain_basic = ITemplate.langchain_basic.value
+    full = ITemplate.full.value
     template_type: str = questionary.select(
-        "Choose a template", choices=[ITemplate.basic, ITemplate.langchain_basic ,questionary.Choice(ITemplate.full, disabled=disabled_message)]
+        "Choose a template", choices=[
+            basic,
+            langchain_basic,
+            questionary.Choice(full, disabled=disabled_message)
+        ]
     ).ask()
     if template_type == ITemplate.full:
         questionary.select(
